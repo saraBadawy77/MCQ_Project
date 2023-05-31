816c62e58cae933bc1b8273a436f3430eb8f0c99
AhmedAboAlata1 <62518605+ahmedAboalata1@users.noreply.github.com>
2023-06-01 00:00:33 +0300

    Server&Login&Register StructureOnly

--- MSQ_PROJ/src/app/app.module.ts
+++ MSQ_PROJ/src/app/app.module.ts
@@ -5,18 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { WelcomePageComponent } from './shared/welcome-page/welcome-page.component';
-import { TeacherFormComponent } from './Teacher/Teacher_form/teacher-form/teacher-form.component';
-import { StudentFormComponent } from './strudent/student-form/student-form.component';
 import { LoginFormComponent } from './shared/login-form/login-form.component';
+import { RegisterComponent } from './shared/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     WelcomePageComponent,
-    TeacherFormComponent,
-    StudentFormComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
