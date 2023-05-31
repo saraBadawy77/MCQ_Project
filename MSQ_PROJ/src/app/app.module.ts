e9371310dccad8eb80c2b60bb53bca5afa1e0cb3
AhmedAboAlata1 <62518605+ahmedAboalata1@users.noreply.github.com>
2023-05-31 23:20:02 +0300

    Remove FormS &only one login Form

--- MSQ_PROJ/src/app/app.module.ts
+++ MSQ_PROJ/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { WelcomePageComponent } from './shared/welcome-page/welcome-page.component';
 import { TeacherFormComponent } from './Teacher/Teacher_form/teacher-form/teacher-form.component';
+import { StudentFormComponent } from './strudent/student-form/student-form.component';
+import { LoginFormComponent } from './shared/login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     WelcomePageComponent,
-    TeacherFormComponent
+    TeacherFormComponent,
+    StudentFormComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
