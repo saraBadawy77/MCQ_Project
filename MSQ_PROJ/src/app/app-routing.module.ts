e3f4b6ce6f22b0eaca5e3b85851575b931173599
AhmedAboAlata1 <62518605+ahmedAboalata1@users.noreply.github.com>
2023-06-01 15:53:04 +0300

    1

--- MSQ_PROJ/src/app/app-routing.module.ts
+++ MSQ_PROJ/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './shared/login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './shared/register/register.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'login' , component:LoginFormComponent},
+  {path:' Register' , component:RegisterComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
