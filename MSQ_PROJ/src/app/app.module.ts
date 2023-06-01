8bdc44d2e7eeb6e7422f6eea11430a486c8413b9
AhmedAboAlata1 <62518605+ahmedAboalata1@users.noreply.github.com>
2023-06-01 03:34:38 +0300

    Add Angular Material

--- MSQ_PROJ/src/app/app.module.ts
+++ MSQ_PROJ/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { WelcomePageComponent } from './shared/welcome-page/welcome-page.component';
 import { LoginFormComponent } from './shared/login-form/login-form.component';
 import { RegisterComponent } from './shared/register/register.component';
+import {MatRadioModule} from '@angular/material/radio'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    
     WelcomePageComponent,
     LoginFormComponent,
+    
     RegisterComponent
   ],
   imports: [
+    MatSlideToggleModule,
+    MatRadioModule,
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
