6830e84cd29f72dd906e1b7110ca9f3d72b4b02d
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2023-06-01 16:15:43 +0300

    g

--- MSQ_PROJ/src/app/shared/register/register.component.ts
+++ MSQ_PROJ/src/app/shared/register/register.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit{
-
-  registForm =new FormGroup({
-    studentName:new FormControl("",[Validators.required,Validators.pattern('^[a-zA-Z]+$'),Validators.minLength(20)]),
-    studentEmail:new FormControl("",Validators.required),
-    studentPassword : new FormControl("",[Validators.required,Validators.minLength(5)]),
-    
-
-  })
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
+export class RegisterComponent {
 
 }
