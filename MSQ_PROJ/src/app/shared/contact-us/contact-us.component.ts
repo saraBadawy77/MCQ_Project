33c636a8dec6097ad5728e618129e332c2d21c4f
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2023-06-02 19:25:44 +0300

    dfghj

--- MSQ_PROJ/src/app/shared/contact-us/contact-us.component.ts
+++ MSQ_PROJ/src/app/shared/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUSComponent {
+  contactForm=new FormGroup({
+    name:new FormControl("",[Validators.required,Validators.pattern('^[a-zA-Z]+$'),Validators.minLength(3)]),
+    email:new FormControl("",[Validators.required,Validators.email]),
+    message:new FormControl("",Validators.required)
+  })
+
+  // get getName(){
+  //   return this.contactForm.controls["name"];
+  // }
+   
+  // get getEmail(){
+  //   return this.contactForm.controls["email"];
+  // }
+
+  // get getMessage(){
+  //   return this.contactForm.controls["message"];
+  // }
+  sname:any=this.contactForm.controls.name;
+  email:any=this.contactForm.controls.email;
+  message:any=this.contactForm.controls.message;
 
 }
